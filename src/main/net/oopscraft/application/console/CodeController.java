f7b4da541bafe6a0abeb4048cdfe0f7684f6aada
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-01-15 21:34:35 +0900

    .

--- src/main/net/oopscraft/application/console/CodeController.java
+++ src/main/net/oopscraft/application/console/CodeController.java
@@ -6,7 +6,7 @@
  * Released under the LGPL-3.0 licence
  * https://opensource.org/licenses/lgpl-3.0.html
  */
-package net.oopscraft.application.console.controller;
+package net.oopscraft.application.console;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
