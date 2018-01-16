4c300d5a9b0fef425e7871a10b94e0b56681840a
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-01-17 00:24:58 +0900

    .

--- src/main/net/oopscraft/application/console/ConsoleSecurityFilter.java
+++ src/main/net/oopscraft/application/console/ConsoleSecurityFilter.java
@@ -1,5 +1,42 @@
 package net.oopscraft.application.console;
 
-public class ConsoleSecurityFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.oopscraft.application.user.UserSecurityFilter;
+
+public class ConsoleSecurityFilter implements Filter {
+	
+	private static final Log LOG = LogFactory.getLog(UserSecurityFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    	HttpServletRequest request = (HttpServletRequest) req;
+    	HttpServletResponse response = (HttpServletResponse) res;
+        LOG.debug(String.format("IP:%s,URL:%s,URI:%s",request.getRemoteAddr(),request.getRequestURL(),request.getRequestURI()));
+        
+        // checking session
+        if(!request.getRequestURI().contains(request.getContextPath() + "/console/login")) {
+	        HttpSession session = request.getSession(false);
+	        if(session == null || session.getAttribute("admin") == null) {
+	        	response.sendRedirect(request.getContextPath() + "/console/login");
+	        	return;
+	        }
+        }
+        
+        // doFilter
+        chain.doFilter(req, res);
+	}
 
 }
--- src/main/net/oopscraft/application/user/UserSecurityFilter.java
+++ src/main/net/oopscraft/application/user/UserSecurityFilter.java
@@ -14,7 +14,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
  
-@Component
 public class UserSecurityFilter implements Filter {
 	
 	private static final Log LOG = LogFactory.getLog(UserSecurityFilter.class);
