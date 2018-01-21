5cc7e4e494e073ce7b32cc9fa9d68b40788b6677
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-01-22 00:47:14 +0900

    .

--- src/test/net/oopscraft/application/ApplicationTest.java
+++ src/test/net/oopscraft/application/ApplicationTest.java
@@ -3,21 +3,12 @@ package net.oopscraft.application;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(ApplicationJUnit4ClassRunner.class)
-@ContextConfiguration(locations="file:src/test/net/oopscraft/application/spring.xml")
+@ContextConfiguration(locations="file:src/test/net/oopscraft/application/spring-test.xml")
 @WebAppConfiguration
+@Transactional
 public class ApplicationTest {
-	
-//	@Before
-//	public void before() {
-//		try {
-//			ApplicationContainer.launchForTest(Application.class, new String[] {});
-//		}catch(Exception e){
-//			e.printStackTrace(System.err);
-//			assert(false);
-//		}
-//	}
-
 
 }
