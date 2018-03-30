735ccc177f52b79dd7193e0e6eb50dfcbc70bace
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-03-31 03:11:54 +0900

    .

--- src/main/net/oopscraft/application/core/security/captcha/Captcha.java
+++ src/main/net/oopscraft/application/core/security/captcha/Captcha.java
@@ -104,15 +104,7 @@ public class Captcha implements Serializable {
 		int frameLength = 0;
 		for(int i = 0; i < charArray.length; i ++) {
 			char c = charArray[i];
-			String soundFileName = "sound/" + c + ".wav";
-			LOG.debug("+ soundFileName:" + soundFileName);
-			LOG.debug("+ getResource:" + this.getClass().getResource(soundFileName));
-			LOG.debug("+ getResourceAsStream:" + this.getClass().getResourceAsStream(soundFileName));
-
-//			soundFileName = "net/oopscraft/application/core/security/dao/mysql/UserDao.xml";
-//			LOG.debug("+ soundFileName:" + soundFileName);
-//			LOG.debug("+ getResource:" + this.getClass().getClassLoader().getResource(soundFileName));
-//			LOG.debug("+ getResourceAsStream:" + this.getClass().getClassLoader().getResourceAsStream(soundFileName));
+			String soundFileName = "sound" + File.separator + c + ".wav";
 			AudioInputStream ais = AudioSystem.getAudioInputStream(this.getClass().getResource(soundFileName));
 			audioInputStreamList.add(ais);
 			if(audioFormat == null) {
