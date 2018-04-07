0b5546ca6633f1ced7871f46aa7b32ac01baf8eb
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-04-08 00:57:03 +0900

    .

--- src/main/net/oopscraft/application/console/ConsoleSecurityFilter.java
+++ src/main/net/oopscraft/application/console/ConsoleSecurityFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -37,6 +38,24 @@ public class ConsoleSecurityFilter implements Filter {
         
         // doFilter
         chain.doFilter(req, res);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
