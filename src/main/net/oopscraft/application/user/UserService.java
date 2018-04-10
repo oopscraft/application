652543b171177faefc6d6e815220d167bfc3d120
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-04-10 23:45:23 +0900

    .

--- src/main/net/oopscraft/application/user/UserService.java
+++ src/main/net/oopscraft/application/user/UserService.java
@@ -8,154 +8,25 @@
  */
 package net.oopscraft.application.user;
 
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import net.oopscraft.application.ApplicationContext;
-import net.oopscraft.application.core.SqlSessionProxy;
-import net.oopscraft.application.core.SqlSessionProxyFactory;
-import net.oopscraft.application.user.dao.UserDao;
 
 /**
  * @author chomookun@gmail.com
  *
  */
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Log LOG = LogFactory.getLog(UserService.class);
-	SqlSessionProxyFactory sqlSessionFactory;
 	
-	public UserService() throws Exception {
-		sqlSessionFactory = ApplicationContext.getInstance().getSqlSessionProxyFactory("application");
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+		
 	}
-	
-	/**
-	 * getting user list
-	 * @param user
-	 * @param rows
-	 * @param page
-	 * @return
-	 * @throws Exception
-	 */
-	public List<User> getUserList(User user, int rows, int page) throws Exception {
-		List<User> userList = null;
-		SqlSessionProxy sqlSession = null;
-		try {
-			sqlSession = sqlSessionFactory.openSession();
-			UserDao userDao = sqlSession.getMapper(UserDao.class);
-			userList = userDao.selectUserList(user, rows, page);
-		}catch(Exception e) {
-			LOG.error(e.getMessage(),e);
-			sqlSession.rollback();
-			throw e;
-		}finally {
-			sqlSession.close();
-		}
-		return userList;
-	}
-	
-	/**
-	 * getting user
-	 * @param id
-	 * @return
-	 * @throws Exception
-	 */
-	public User getUser(String id) throws Exception {
-		User user = null;
-		SqlSessionProxy sqlSession = null;
-		try {
-			sqlSession = sqlSessionFactory.openSession();
-			UserDao userDao = sqlSession.getMapper(UserDao.class);
-			user = userDao.selectUser(id);
-		}catch(Exception e) {
-			LOG.error(e.getMessage(),e);
-			sqlSession.rollback();
-			throw e;
-		}finally {
-			sqlSession.close();
-		}
-		return user;
-	}
-	
-	/**
-	 * saveUser
-	 * @param user
-	 * @return User
-	 * @throws Exception
-	 */
-	public User saveUser(User user) throws Exception {
-		SqlSessionProxy sqlSession = null;
-		try {
-			sqlSession = sqlSessionFactory.openSession();
-			UserDao userDao = sqlSession.getMapper(UserDao.class);
-			if(userDao.selectUser(user.getId()) == null) {
-				userDao.insertUser(user);
-			}else {
-				userDao.updateUser(user);
-			}
-			user = userDao.selectUser(user.getId());
-			sqlSession.commit();
-		}catch(Exception e) {
-			LOG.error(e.getMessage(),e);
-			sqlSession.rollback();
-			throw e;
-		}finally {
-			sqlSession.close();
-		}
-		return user;
-	}
-	
-	/**
-	 * removeUser
-	 * @param id
-	 * @return User
-	 * @throws Exception
-	 */
-	public User removeUser(String id) throws Exception {
-		User user = null;
-		SqlSessionProxy sqlSession = null;
-		try {
-			sqlSession = sqlSessionFactory.openSession();
-			UserDao userDao = sqlSession.getMapper(UserDao.class);
-			user = userDao.selectUser(id);
-			userDao.deleteUser(id);
-			sqlSession.commit();
-		}catch(Exception e) {
-			LOG.error(e.getMessage(),e);
-			sqlSession.rollback();
-			throw e;
-		}finally {
-			sqlSession.close();
-		}
-		return user;
-	}
-
-	/**
-	 * getUserGroupList
-	 * @param user
-	 * @return
-	 * @throws Exception
-	 */
-	public List<Group> getUserGroupList(User user) throws Exception {
-		List<Group> userGroupList = null;
-		SqlSessionProxy sqlSession = null;
-		try {
-			sqlSession = sqlSessionFactory.openSession();
-			UserDao userDao = sqlSession.getMapper(UserDao.class);
-			userGroupList = userDao.selectUserGroupList(user);
-		}catch(Exception e) {
-			LOG.error(e.getMessage(),e);
-			sqlSession.rollback();
-			throw e;
-		}finally {
-			sqlSession.close();
-		}
-		return userGroupList;
-	}
-
-
 }
--- src/main/net/oopscraft/application/user/controller/SignController.java
+++ src/main/net/oopscraft/application/user/controller/SignController.java
@@ -8,10 +8,46 @@
  */
 package net.oopscraft.application.user.controller;
 
-/**
- * @author chomookun@gmail.com
- *
- */
-public class SignController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user/sign")
+public class SignController {
+	
+	@Autowired
+	HttpServletRequest request;
+	
+	@Autowired
+	HttpServletResponse response;
+	
+	@RequestMapping(value="", method=RequestMethod.GET) 
+	public ModelAndView get() throws Exception {
+		ModelAndView modelAndView = new ModelAndView("user/sign.jsp");
+		return modelAndView;
+	}
+	
+//	@RequestMapping(value="", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	public ResponseEntity<?> post(
+//		 @RequestParam("id")String admin
+//		,@RequestParam("password")String password
+//	) throws Exception {
+//		return new ResponseEntity<>(null, HttpStatus.OK);
+//	}
+//	
+//	@RequestMapping(value="", method=RequestMethod.DELETE) 
+//	public ModelAndView delete() throws Exception {
+//		request.getSession().invalidate();
+//		return new ModelAndView("redirect:/console");
+//	}
 }
