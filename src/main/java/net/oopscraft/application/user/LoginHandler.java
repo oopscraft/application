51e7dbfee3bb86b9be6bcdf823747458a0f51f55
chomookun <chomookun@gmail.com>
2018-08-08 13:09:51 +0000

    Create LoginHandler.java
    .

---
+++ src/main/java/net/oopscraft/application/user/LoginHandler.java
@@ -0,0 +1,38 @@
+public class LoginHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
+	
+	/**
+	 * 인증 성공시 호출 핸들러
+	 * @param HttpServletRequest, HttpServletResponse, Authentication
+	 * @return void
+	 */
+	@Override
+	public void onAuthenticationSuccess(
+		 HttpServletRequest request
+		,HttpServletResponse response
+		,Authentication authentication
+	) throws IOException, ServletException {
+		ObjectMapper om = new ObjectMapper();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("success", true);
+		String jsonString = om.writeValueAsString(map);
+		OutputStream out = response.getOutputStream();
+		out.write(jsonString.getBytes());
+	}
+	
+	/**
+	 * 인증 실패시 호출 핸들러
+	 * @param HttpServletRequest, HttpServletResponse, AuthenticationException
+	 * @return void
+	 */
+	@Override
+	public void onAuthenticationFailure(
+		HttpServletRequest request, 
+		HttpServletResponse response,
+		AuthenticationException exception
+	) throws IOException, ServletException {
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		OutputStream out = response.getOutputStream();
+		out.write(exception.getMessage().getBytes());
+	}
+
+}
