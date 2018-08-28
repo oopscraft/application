3fa3239deead23d94a636b10b30630fa7c68c47d
chomookun <chomookun@gmail.com>
2018-08-28 11:08:29 +0000

    Update Pageable.java
    .

--- src/main/java/net/oopscraft/application/core/Pageable.java
+++ src/main/java/net/oopscraft/application/core/Pageable.java
@@ -1,4 +1,4 @@
-package net.oopscraft.application.core;
+package net.lotte.chamomile.batch.core;
 
 import org.apache.ibatis.session.RowBounds;
 
@@ -6,53 +6,99 @@ public class Pageable extends RowBounds {
 	
 	int rows = Integer.MAX_VALUE;
 	int page = 1;
-	int totalRows = -1;
-	boolean enableTotalRows = false;
+	int offset = 0;
+	int limit = Integer.MAX_VALUE;
+	int sqlOffset = 0;
+	int sqlLimit = Integer.MAX_VALUE;
+	boolean enableTotalCount = false;
+	int totalCount = -1;
 	
 	public Pageable(int rows, int page) {
 		this.rows = rows;
 		this.page = page;
+		this.offset = (this.rows*this.page) - this.rows;
+		this.limit = rows;
+		this.sqlOffset = (this.rows*this.page) - this.rows;
+		this.sqlLimit = rows;
 	}
 	
-	public int getLimit() {
-		return this.rows;
-	}
-	
-	public int getOffset() {
-		int offset = (this.rows*this.page) - this.rows;
-		return offset;
+	public int getPage() {
+		return this.page;
 	}
 	
 	public int getRows() {
-		return rows;
-	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	public void enableTotalRows(boolean enableTotalRows) {
-		this.enableTotalRows = enableTotalRows;
+		return this.rows;
 	}
 	
-	public boolean enableTotalRows() {
-		return this.enableTotalRows;
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	
+	public int getOffset() {
+		return this.offset;
+	}
+	
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+	public int getLimit() {
+		return this.limit;
+	}
+	
+	public int getSqlOffset() {
+		return (this.rows*this.page) - this.rows;
+	}
+	
+	public int getSqlLimit() {
+		return this.rows;
 	}
 
-	public int getTotalRows() {
-		return totalRows;
+	public void enableTotalCount(boolean enableTotalCount) {
+		this.enableTotalCount = enableTotalCount;
 	}
-
-	public void setTotalRows(int totalRows) {
-		this.totalRows = totalRows;
+	
+	public boolean enableTotalCount() {
+		return this.enableTotalCount;
+	}
+	
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	
+	public long getTotalCount() {
+		return this.totalCount;
+	}
+	
+	/**
+	 * Return content range value.
+	 * rows, page 기준으로 계산한다.(실제 offset, limit는 SQL로 조회되는 경우 interceptor에서 변경될수 있음)
+	 * @return
+	 */
+	public String toContentRange() {
+		StringBuffer contentRange = new StringBuffer();
+		
+		// start
+		int start = (this.rows*this.page) - this.rows + 1;
+		contentRange.append(start);
+		
+		// end
+		contentRange.append("-");
+		int end = start -1 + this.rows;
+		if(end < this.getTotalCount()) {
+			contentRange.append(end);
+		}else {
+			contentRange.append(this.getTotalCount());
+		}
+		
+		// size
+		contentRange.append("/");
+		if(this.enableTotalCount() == true) {
+			contentRange.append(this.getTotalCount());
+		}else {
+			contentRange.append("*");
+		}
+		return contentRange.toString();
 	}
 
 }
