ff1fbb168c05e689de014fac157895436026b08e
chomookun <chomookun@DESKTOP-VSP7JJC>
2018-10-26 01:29:12 +0900

    .

--- src/main/java/net/oopscraft/application/Application.java
+++ src/main/java/net/oopscraft/application/Application.java
@@ -1,16 +1,71 @@
 package net.oopscraft.application;
 
+import java.io.File;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import javax.sql.DataSource;
+
 import net.oopscraft.application.core.WebServer;
 
 public class Application {
 	
-	WebServer webServer;
+	File contextXmlFile;
+	File contextPropertiesFile;
+	HashMap<String,WebServer> webServers = new LinkedHashMap<String,WebServer>();
+	HashMap<String,DataSource> dataSources = new LinkedHashMap<String,DataSource>();
 	
-	public WebServer getWebServer() {
-		return webServer;
+	final void setContextXmlFile(File contextXmlFile) throws Exception {
+		this.contextXmlFile = contextXmlFile;
 	}
-	public void setWebServer(WebServer webServer) {
-		this.webServer = webServer;
+	
+	final void setContextPropertiesFile(File contextPropertiesFile) {
+		this.contextPropertiesFile = contextPropertiesFile;
+	}
+	
+	final void setWebServers(HashMap<String,WebServer> webServers) {
+		this.webServers = webServers;
+	}
+	
+	final void setWebServer(String id, WebServer webServer) {
+		webServers.put(id, webServer);
+	}
+
+	public final HashMap<String,WebServer> getWebServers() {
+		return webServers;
+	}
+	
+	public final WebServer getWebServer(String id) {
+		return webServers.get(id);
+	}
+
+	final void setDataSource(String id, DataSource dataSource) {
+		dataSources.put(id, dataSource);
+	}
+	
+	public final void setDataSources(HashMap<String,DataSource> dataSources) {
+		this.dataSources = dataSources;
+	}
+	
+	public final HashMap<String,DataSource> getDataSources() {
+		return dataSources;
+	}
+	
+	public final DataSource getDataSource(String id) {
+		return dataSources.get(id);
+	}
+	
+	public final void start() throws Exception {
+		
+		Connection connection = this.getDataSource("oltp").getConnection();
+		System.err.println(connection);
+		
+		onStart();
+	}
+	
+	public final void stop() throws Exception {
+		onStop();
 	}
 	
 	/**
@@ -28,5 +83,6 @@ public class Application {
 	public void onStop() throws Exception {
 		// void
 	};
+
 
 }
