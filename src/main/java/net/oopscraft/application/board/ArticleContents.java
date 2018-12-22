55772460783dbc62e5fdce6ab5ce33eb5e24ddfe
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-12-22 16:59:34 +0900

    .

---
+++ src/main/java/net/oopscraft/application/board/ArticleContents.java
@@ -0,0 +1,52 @@
+package net.oopscraft.application.board;
+
+import java.io.Serializable;
+
+public class ArticleContents {
+
+	public static class Pk implements Serializable {
+		private static final long serialVersionUID = 3127781407229494383L;
+		public Pk() {}
+		public Pk(String boardId, long no) {
+			this.boardId = boardId;
+			this.no = no;
+		}
+		String boardId;
+		long no;
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(obj instanceof Pk) {
+				Pk pk = (Pk)obj;
+				if(this.getBoardId().equals(pk.getBoardId())
+				&& this.getNo() == pk.getNo()
+				) {
+					return true;
+				}else {
+					return false;
+				}
+			}else {
+				return false;
+			}
+		}	
+		
+		@Override
+		public int hashCode() {
+			return (boardId + Long.toString(no)).hashCode();
+		}
+		
+		public String getBoardId() {
+			return boardId;
+		}
+		public void setBoardId(String boardId) {
+			this.boardId = boardId;
+		}
+		public long getNo() {
+			return no;
+		}
+		public void setNo(long no) {
+			this.no = no;
+		}
+	}
+	
+}
