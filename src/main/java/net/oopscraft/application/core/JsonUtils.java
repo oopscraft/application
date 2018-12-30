38d35dd1bf9f23fd33def89836fc156963aad9fe
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2018-12-30 22:20:28 +0900

    .

--- src/main/java/net/oopscraft/application/core/JsonUtils.java
+++ src/main/java/net/oopscraft/application/core/JsonUtils.java
@@ -10,41 +10,42 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 
 public class JsonUtils {
 	
+	public static ObjectMapper objectMapper = new ObjectMapper();
+	static {
+		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
+		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
+	
+	public static ObjectMapper getObjectMapper() {
+		return objectMapper;
+	}
+	
 	public static String toJson(Object obj) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
-		return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+		return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
 	}
 	
 	public static String toJson(List<?> list) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
-		return mapper.writeValueAsString(list);
+		return objectMapper.writeValueAsString(list);
 	}
 	
 	public static <T> T toObject(String json, Class<T> clazz) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return mapper.readValue(json, clazz);
+		return objectMapper.readValue(json, clazz);
 	}
 	
 	public static <T> List<T> toList(String json, Class<T> clazz) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return mapper.readValue(json, TypeFactory.defaultInstance().constructCollectionType(ArrayList.class, clazz));
+		return objectMapper.readValue(json, TypeFactory.defaultInstance().constructCollectionType(ArrayList.class, clazz));
 	}
 	
 	public static String stringify(String string) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(string);
-		return mapper.writeValueAsString(json);
+		String json = objectMapper.writeValueAsString(string);
+		return objectMapper.writeValueAsString(json);
 	}
 	
 	public static boolean isJson(String value) {
 	    boolean valid = true;
 	    try{
-	    	ObjectMapper mapper = new ObjectMapper();
-	    	mapper.readTree(value);
+	    	objectMapper.readTree(value);
 	    } catch(Exception e){
 	        valid = false;
 	    }
