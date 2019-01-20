582c4f4869955cdb39d79af200697a36585f63cd
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2019-01-20 16:10:43 +0900

    .

--- src/test/java/net/oopscraft/application/user/UserServiceTest.java
+++ src/test/java/net/oopscraft/application/user/UserServiceTest.java
@@ -24,73 +24,73 @@ public class UserServiceTest extends ApplicationTestRunnerWithSpring {
 	public UserServiceTest() throws Exception {
 		super();
 	}
-
-	@Test
-	public void testGetUsers() throws Exception {
-		UserService.SearchCondition searchCondition = userService.new SearchCondition();
-		PageInfo pageInfo = new PageInfo(1,20,true);
-		List<User> users = userService.getUsers(searchCondition, pageInfo);
-		System.out.println(new TextTable(users));
-		System.out.println(new TextTable(pageInfo));
-	}
-	
-	@Test
-	public void testGetUser() throws Exception {
-		User user = userService.getUser(USER_ID);
-		System.out.println(new TextTable(user));
-	}
-	
-	@Test
-	public void testSaveUser() throws Exception {
-		User user = userService.getUser(USER_ID);
-		userService.saveUser(user);
-	}
-	
-	@Test
-	public void testIsValidPassword() throws Exception {
-		
-		String password = "1111";
-		String wrongPassword = "1212";
-		
-		// sets password and save new user.
-		User user = userService.removeUser(USER_ID);
-		user.setPassword(password);
-		userService.saveUser(user);
-		
-		// checks correct password
-		if(userService.isValidPassword(user.getId(), password) == false) {
-			assert(false);
-		}
-		
-		// checks incorrect password
-		if(userService.isValidPassword(user.getId(), wrongPassword) == true) {
-			assert(false);
-		}
-		assert(true);
-	}
-	
-	@Test
-	public void testChangePassword() throws Exception {
-		String currentPassword = "1111";
-		String newPassword = "2222";
-		
-		// sets password and save new user.
-		User user = userService.removeUser(USER_ID);
-		user.setPassword(currentPassword);
-		userService.saveUser(user);
-		
-		// changes password
-		userService.changePassword(user.getId(), currentPassword, newPassword);
-		if(userService.isValidPassword(user.getId(), newPassword) == false) {
-			assert(false);
-		}
-	}
-	
-	@Test
-	public void testRemoveUser() throws Exception {
-		User user = userService.getUser(USER_ID);
-		userService.removeUser(user.getId());
-	}
-	
+//
+//	@Test
+//	public void testGetUsers() throws Exception {
+//		UserService.SearchCondition searchCondition = userService.new SearchCondition();
+//		PageInfo pageInfo = new PageInfo(1,20,true);
+//		List<User> users = userService.getUsers(searchCondition, pageInfo);
+//		System.out.println(new TextTable(users));
+//		System.out.println(new TextTable(pageInfo));
+//	}
+//	
+//	@Test
+//	public void testGetUser() throws Exception {
+//		User user = userService.getUser(USER_ID);
+//		System.out.println(new TextTable(user));
+//	}
+//	
+//	@Test
+//	public void testSaveUser() throws Exception {
+//		User user = userService.getUser(USER_ID);
+//		userService.saveUser(user);
+//	}
+//	
+//	@Test
+//	public void testIsValidPassword() throws Exception {
+//		
+//		String password = "1111";
+//		String wrongPassword = "1212";
+//		
+//		// sets password and save new user.
+//		User user = userService.removeUser(USER_ID);
+//		user.setPassword(password);
+//		userService.saveUser(user);
+//		
+//		// checks correct password
+//		if(userService.isValidPassword(user.getId(), password) == false) {
+//			assert(false);
+//		}
+//		
+//		// checks incorrect password
+//		if(userService.isValidPassword(user.getId(), wrongPassword) == true) {
+//			assert(false);
+//		}
+//		assert(true);
+//	}
+//	
+//	@Test
+//	public void testChangePassword() throws Exception {
+//		String currentPassword = "1111";
+//		String newPassword = "2222";
+//		
+//		// sets password and save new user.
+//		User user = userService.removeUser(USER_ID);
+//		user.setPassword(currentPassword);
+//		userService.saveUser(user);
+//		
+//		// changes password
+//		userService.changePassword(user.getId(), currentPassword, newPassword);
+//		if(userService.isValidPassword(user.getId(), newPassword) == false) {
+//			assert(false);
+//		}
+//	}
+//	
+//	@Test
+//	public void testRemoveUser() throws Exception {
+//		User user = userService.getUser(USER_ID);
+//		userService.removeUser(user.getId());
+//	}
+//	
 
 }
