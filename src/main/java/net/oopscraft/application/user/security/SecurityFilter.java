d66f056299449b1562b6cad0e0e6f657d775dc2a
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2019-01-23 01:59:12 +0900

    .

--- src/main/java/net/oopscraft/application/user/security/SecurityFilter.java
+++ src/main/java/net/oopscraft/application/user/security/SecurityFilter.java
@@ -3,48 +3,75 @@ package net.oopscraft.application.user.security;
 import java.io.IOException;
 
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+
+import net.oopscraft.application.core.StringUtils;
+import net.oopscraft.application.user.User;
 
 @Component
 public class SecurityFilter extends GenericFilterBean   {
 	
-	private static Logger LOGGER = LoggerFactory.getLogger(SecurityFilter.class);
+	private final static Logger LOGGER = LoggerFactory.getLogger(SecurityFilter.class);
+	private static final String AUTHORIZATION_HEADER = "Authorization";
 	
 	@Autowired
-	SecurityHandler securityHandler;
+	AccessTokenEncoder accessTokenEncoder;
  
-	@Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     	HttpServletRequest request = (HttpServletRequest) req;
-    	HttpServletResponse response = (HttpServletResponse) res;
         String uri = request.getRequestURI();
         String method = request.getMethod();
         LOGGER.info(String.format("[%s][%s]",  method, uri));
         
-        /*
-        if(1 == 1) {
-        	securityHandler.handle(request, response, new AccessDeniedException("FDSAFDSAFDSA"));
-        	return;
-        }
-        */
+        String authorization = request.getHeader(AUTHORIZATION_HEADER);
+        if(StringUtils.isNotEmpty(authorization)) {
+        	
+            // decode principal
+            try {
+                String token = parseToken(authorization);
+                LOGGER.debug(String.format("token:[%s]", token));
+                User user = accessTokenEncoder.decode(token);
+        		UserDetails userDetails = new UserDetails(user);
+        		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+        		SecurityContext securityContext = SecurityContextHolder.getContext();
+        		securityContext.setAuthentication(authentication);
+            }catch(Exception e) {
+            	AuthenticationException authenticationException = new BadCredentialsException("Invalid Token Claims.");
+            	LOGGER.error(authenticationException.getMessage());
+            	throw authenticationException;
+            }
+        } 
         
-        chain.doFilter(req, res);        	
-        
-
+        // forward
+        chain.doFilter(req, res);
     }
+	
+	/**
+	 * Parse token from authorization header
+	 * @param authorization
+	 * @return
+	 */
+	private String parseToken(String authorization) {
+        if(authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.substring(7, authorization.length());
+            return token;
+        }
+        return null;
+	}
 
 }
