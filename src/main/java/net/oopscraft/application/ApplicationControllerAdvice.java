575f550ee0db7c697ee0e63ca5cb8a9b95ba42b6
chomookun@gmail.com <chomookun@gmail.com@DESKTOP-73NAMQ8>
2019-01-28 21:44:20 +0900

    .

--- src/main/java/net/oopscraft/application/ApplicationControllerAdvice.java
+++ src/main/java/net/oopscraft/application/ApplicationControllerAdvice.java
@@ -2,12 +2,19 @@ package net.oopscraft.application;
 
 import java.util.Map;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import net.oopscraft.application.user.User;
+import net.oopscraft.application.user.security.UserDetails;
 
 @ControllerAdvice
 @Controller
@@ -18,6 +25,17 @@ public class ApplicationControllerAdvice {
 	public ModelAndView index() throws Exception {
 		ModelAndView modelAndView = new ModelAndView("main.tiles");
 		return modelAndView;
+	}
+	
+	@ModelAttribute("__user")
+	public User getUser() throws Exception {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		if(authentication instanceof AnonymousAuthenticationToken) {
+			return new User();
+		}
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		return userDetails.getUser();
 	}
 	
     @ModelAttribute("__config")
