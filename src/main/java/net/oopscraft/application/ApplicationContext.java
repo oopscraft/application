80db4bcd3d159398c5a4bce898800b2ff5c9d1b0
chomookun <chomookun@gmail.com>
2019-03-23 10:32:53 +0900

    .

--- src/main/java/net/oopscraft/application/ApplicationContext.java
+++ src/main/java/net/oopscraft/application/ApplicationContext.java
@@ -1,5 +1,13 @@
 package net.oopscraft.application;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApplicationContext {
+	
+	@Bean
+	public DataSource dataSource() throws Exception {
+		
+	}
 
 }
