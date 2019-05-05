c959c55b0dfa4bd8afa634c74ed95ded4f0527f0
chomookun <chomookun@DESKTOP-D5ACIAC>
2019-05-06 02:44:26 +0900

    V

---
+++ src/main/java/net/oopscraft/application/core/asm/ASMTest.java
@@ -0,0 +1,29 @@
+package net.oopscraft.application.core.asm;
+
+import java.io.IOException;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class ASMTest {
+	
+	public static void main(String[] args) throws Exception {
+		try { 
+			ClassPrinterVisitor cp = new ClassPrinterVisitor(); 
+			ClassReader cr = new ClassReader("net.oopscraft.application.core.asm.Callee"); 
+			cr.accept(cp, 0); 
+			MethodVisitor mv = cp.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+
+			
+		} catch (Throwable t) { 
+			t.printStackTrace(); 
+			throw new RuntimeException(t); 
+		}
+		
+
+	}
+
+
+}
