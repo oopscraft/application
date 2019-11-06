4159e1adcebf1e44a199a59bf27f8bc5bdac12bf
chomookun <chomookun@DESKTOP-D5ACIAC>
2019-11-07 00:49:05 +0900

    .

--- src/main/java/net/oopscraft/application/user/LoginController.java
+++ src/main/java/net/oopscraft/application/user/LoginController.java
@@ -1,4 +1,4 @@
-package net.oopscraft.application.user.controller;
+package net.oopscraft.application.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
