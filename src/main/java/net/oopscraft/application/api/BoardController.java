ceefebd50ad08a6c017c682877f11fde8294cbe5
chomookun <chomookun@chomookun>
2020-02-05 02:04:56 +0900

    .

--- src/main/java/net/oopscraft/application/api/BoardController.java
+++ src/main/java/net/oopscraft/application/api/BoardController.java
@@ -9,13 +9,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import net.oopscraft.application.article.entity.Article;
 import net.oopscraft.application.board.BoardArticleService;
--- src/main/java/net/oopscraft/application/api/MenuController.java
+++ src/main/java/net/oopscraft/application/api/MenuController.java
@@ -7,11 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.oopscraft.application.core.PageInfo;
