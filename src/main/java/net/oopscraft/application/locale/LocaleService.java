ac5214b86917d2055a918441ea402b56e21d6a37
chomookun <chomookun@gmail.com>
2020-03-23 10:36:25 +0900

    .

---
+++ src/main/java/net/oopscraft/application/locale/LocaleService.java
@@ -0,0 +1,126 @@
+package net.oopscraft.application.locale;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Vector;
+
+import org.springframework.stereotype.Component;
+
+import net.oopscraft.application.core.ValueMap;
+
+@Component
+public class LocaleService {
+
+	/**
+	 * Gets available Locale list
+	 * @return
+	 * @throws Exception
+	 */
+	private static List<Locale> getAvailableLocales() throws Exception {
+		List<Locale> locales = new ArrayList<Locale>();
+		for(Locale locale : Locale.getAvailableLocales()) {
+			if(locale.toString().length() == 5) {
+				locales.add(locale);
+			}
+		}
+		return locales;
+	}
+	
+	private static Locale getDefaultLocaleIfNotExist(Locale locale) throws Exception {
+		if(locale == null) {
+			locale = Locale.getDefault();
+		}
+		return locale;
+	}
+	
+	/**
+	 * Returns locale list
+	 * @return
+	 * @throws Exception
+	 */
+	public List<ValueMap> getLocales(Locale inLocale) throws Exception {
+		inLocale = getDefaultLocaleIfNotExist(inLocale);
+		List<ValueMap> locales = new ArrayList<ValueMap>();
+		for(Locale locale : getAvailableLocales()) {
+			ValueMap localeInfo = new ValueMap();
+			localeInfo.set("locale", locale.toString());
+			localeInfo.set("displayName", locale.getDisplayName(inLocale));
+			localeInfo.set("displayNameByLocale", locale.getDisplayName(locale));
+			localeInfo.set("country", locale.getCountry());
+			localeInfo.set("displayCountry", locale.getDisplayCountry(inLocale));
+			localeInfo.set("displayCountryByLocale", locale.getDisplayCountry(locale));
+			localeInfo.set("language", locale.getLanguage());
+			localeInfo.set("displayLanguage", locale.getDisplayLanguage(inLocale));
+			localeInfo.set("displayLanguageByLocale", locale.getDisplayLanguage(locale));
+			locales.add(localeInfo);
+		}
+		locales.sort(new Comparator<ValueMap>() {
+			@Override
+			public int compare(ValueMap o1, ValueMap o2) {
+				return o1.getString("displayName").compareTo(o2.getString("displayName"));
+			}
+		});
+		return locales;
+	}
+	
+	/**
+	 * Returns countries
+	 * @return
+	 * @throws Exception
+	 */
+	public List<ValueMap> getCountries(Locale inLocale) throws Exception {
+		inLocale = getDefaultLocaleIfNotExist(inLocale);
+		List<ValueMap> countries = new ArrayList<ValueMap>();
+		Vector<String> keys = new Vector<String>();
+		for(Locale locale : getAvailableLocales()) {
+			String country = locale.getCountry();
+			if(!keys.contains(country)) {
+				keys.add(country);
+				ValueMap countryInfo = new ValueMap();
+				countryInfo.set("country", country);
+				countryInfo.set("displayCountry", locale.getDisplayCountry(inLocale));
+				countryInfo.set("displayCountryByLocale", locale.getDisplayCountry(locale));
+				countries.add(countryInfo);
+			}
+		}
+		countries.sort(new Comparator<ValueMap>() {
+			@Override
+			public int compare(ValueMap o1, ValueMap o2) {
+				return o1.getString("displayCountry").compareTo(o2.getString("displayCountry"));
+			}
+		});
+		return countries;
+	}
+	
+	/**
+	 * Returns languages
+	 * @return
+	 * @throws Exception
+	 */
+	public List<ValueMap> getLanguages(Locale inLocale) throws Exception {
+		inLocale = getDefaultLocaleIfNotExist(inLocale);
+		List<ValueMap> languages = new ArrayList<ValueMap>();
+		Vector<String> keys = new Vector<String>();
+		for(Locale locale : getAvailableLocales()) {
+			String language = locale.getLanguage();
+			if(!keys.contains(language)) {
+				keys.add(language);
+				ValueMap languageInfo = new ValueMap();
+				languageInfo.set("language", language);
+				languageInfo.set("displayLanguage", locale.getDisplayLanguage(inLocale));
+				languageInfo.set("displayLanguageByLocale", locale.getDisplayName(locale).replaceAll("\\(.*\\)", "").trim());
+				languages.add(languageInfo);
+			}
+		}
+		languages.sort(new Comparator<ValueMap>() {
+			@Override
+			public int compare(ValueMap o1, ValueMap o2) {
+				return o1.getString("displayLanguage").compareTo(o2.getString("displayLanguage"));
+			}
+		});
+		return languages;
+	}
+	
+}
---
+++ src/main/resources/META-INF/resources/static/application.js
@@ -0,0 +1,72 @@
+/**
+ * JQUERY AJAX setting
+ */
+var __progress = null;
+$(document).ajaxStart(function(event) {
+	console.debug('$(document).ajaxStart',event);
+	__progress = new duice.Progress(document.body);
+	__progress.start();
+});
+// If not configure, "Provisional headers are shown" error found in CHROME.
+$(document).ajaxSend(function(event, jqXHR, settings) {
+	console.debug('$(document).ajaxSend',event,jqXHR,settings);
+	var csrfToken = duice.getCookie('XSRF-TOKEN');
+	jqXHR.setRequestHeader("X-XSRF-TOKEN",csrfToken);
+	jqXHR.setRequestHeader('Cache-Control','no-cache, no-store, must-revalidate');
+	jqXHR.setRequestHeader('Pragma','no-cache');
+	jqXHR.setRequestHeader('Expires','0');
+});
+// Checks error except cancellation(readyState = 0)
+$(document).ajaxError(function(event, jqXHR, settings,thrownError ){
+	console.debug('$(document).ajaxError');
+	if(jqXHR.readyState > 0){
+		console.error(event, jqXHR, settings, thrownError);
+		if(jqXHR.status === 401){
+			location.reload();
+		}else{
+			duice.alert(jqXHR.responseText);	
+		}
+	}
+});
+// Checks
+$(document).ajaxSuccess(function(event, jqXHR, settings){
+	console.debug('$(document).ajaxSuccess',event,jqXHR,settings);
+});
+// Checks
+$(document).ajaxComplete(function(event, jqXHR, settings){
+	console.debug('$(document).ajaxComplete',event,jqXHR,settings);
+});
+// checks stop event
+$(document).ajaxStop(function(event) {
+	console.debug('$(document).ajaxStop',event);
+	if(__progress){
+		__progress.stop();
+	}
+});
+
+/**
+ * Parsed total count from Content-Range header
+ * @Param {Object} jqXHR
+ */
+const __parseTotalCount = function(jqXHR){
+	var totalCount = -1;
+	var contentRange = jqXHR.getResponseHeader("Content-Range");
+	try {
+    	var totalCount = contentRange.split(' ')[1].split('/')[1];
+		totalCount = parseInt(totalCount);
+		if(isNaN(totalCount)){
+			return -1;
+		}
+		return totalCount;
+	}catch(e){
+		console.error(e);
+		return -1;
+	}
+}
+
+/**
+ * Changes language
+ */ 
+const __changeLanguage = function(language){
+	window.location = '?__lang=' + language;
+}
