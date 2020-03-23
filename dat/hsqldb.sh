53846ee1c73c5d87b7cd5db29eaccadb9ecfe8f1
chomookun <chomookun@gmail.com>
2020-03-23 16:44:52 +0900

    .

--- dat/hsqldb.sh
+++ dat/hsqldb.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-java -cp hsqldb.jar org.hsqldb.server.Server -database.0 file:application -dbname.0 application
+
+function start() {
+	java -cp hsqldb.jar org.hsqldb.server.Server -database.0 file:application -dbname.0 application
+}
+
+function stop() {
+	java -jar sqltool.jar --sql "SHUTDOWN;" application
+}
+
+case ${1} in 
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	*)
+		echo "Usage: ${0} [start|stop]"
+		;;
+esac
+
+exit 0
