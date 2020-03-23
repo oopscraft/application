d93b54cab358265cb44fc0b6667520eefcd5619f
chomookun <chomookun@gmail.com>
2020-03-23 17:15:57 +0900

    .

--- dat/hsqldb.sh
+++ dat/hsqldb.sh
@@ -5,19 +5,23 @@ PID_FILE=${APP_NAME}.pid
 touch ${PID_FILE}
 PID=$(cat ${PID_FILE})
 
+# start
 function start() {
 	java -cp hsqldb.jar org.hsqldb.server.Server -database.0 file:application -dbname.0 application &
 	echo $! > ${PID_FILE}
 }
 
+# status
 function status() {
-	ps -f ${PID} | grep java | grep hsqldb.jar org.hsqldb.server.Server
+	ps -f ${PID} | grep java | grep "hsqldb.jar org.hsqldb.server.Server"
 }
 
+# stop
 function stop() {
-	java -jar sqltool.jar --inlineRc=url=jdbc:hsqldb:hsql://127.0.0.1/application,user=SA --sql "shutdown;"
+	java -jar sqltool.jar --inlineRc=url=jdbc:hsqldb:hsql://127.0.0.1/application,user=SA,password= --sql "shutdown;"
 }
 
+# main
 case ${1} in 
 	start)
 		start
