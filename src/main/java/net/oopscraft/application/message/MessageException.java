f8704731bb7a3db8510822c5a54de068258bd4e2
chomookun <chomookun@gmail.com>
2020-04-20 23:52:11 +0900

    .

---
+++ src/main/java/net/oopscraft/application/message/MessageException.java
@@ -0,0 +1,11 @@
+package net.oopscraft.application.message;
+
+public class MessageException extends Throwable {
+
+	private static final long serialVersionUID = 668103176640687919L;
+	
+	public MessageException() {
+		super();
+	}
+	
+}
