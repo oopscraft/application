552a0bbd773d38b43fb8b93302d505ef2339a281
chomookun <chomookun@gmail.com>
2020-05-23 00:19:20 +0900

    .

---
+++ src/main/java/net/oopscraft/application/api/PageRestController.java
@@ -0,0 +1,46 @@
+package net.oopscraft.application.api;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
+
+import net.oopscraft.application.page.Page;
+import net.oopscraft.application.page.PageService;
+import net.oopscraft.application.security.UserDetails;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/pages")
+public class PageRestController {
+
+	@Autowired
+	PageService pageService;
+	
+	@Autowired
+	LocaleResolver localeResolver;
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Page getMenu(@PathVariable("id") String id) throws Exception {
+		Page page = pageService.getPage(id);
+		
+		// translate menu
+		Locale locale = localeResolver.resolveLocale(request);
+		if(locale != null) {
+			page.setContents(page.getContents(locale.getLanguage()));
+		}
+		
+		// returns page.
+		return page;
+	}
+	
+}
