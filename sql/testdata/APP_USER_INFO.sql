34322fa9ddda2755dd9e6d25ace68d3c60660308
chomookun <chomookun@gmail.com>
2020-05-24 02:06:49 +0900

    .

--- sql/testdata/APP_USER_INFO.sql
+++
@@ -1,4 +0,0 @@
--- App User Info
-INSERT INTO APP_USER_INFO (USER_ID, SYS_EMBD_YN, SYS_INST_DATE, SYS_INST_USER_ID, SYS_UPDT_DATE, SYS_UPDT_USER_ID, USER_EMIL, USER_PHON, USER_PASS, USER_NAME, USER_NICK, USER_STAT, USER_AVAT, USER_SIGN, USER_LOCL, JOIN_DATE, CLOS_DATE) VALUES ('admin', 'Y', '', '', '', '', 'admin@oopscraft.net', '082-010-1234-1234', 'password', 'Administrator', 'Admin', 'ACTIVE', '', '', 'en_US', '', '');
-
-
